e50e1368036ae40989eb662e518b7d9c132c1a76
eneepo <eneepo@gmail.com>
2018-06-04 18:14:42 +0430

    Fix no child admin site was registered

--- blocks/__init__.py
+++ blocks/__init__.py
@@ -1,1 +1,5 @@
-default_app_config = 'blocks.apps.BlocksConfig'
+from django.conf import settings
+
+for block_name in getattr(settings, "BLOCKS_APPS", None):
+    if block_name not in settings.INSTALLED_APPS:
+        settings.INSTALLED_APPS += (block_name, )
